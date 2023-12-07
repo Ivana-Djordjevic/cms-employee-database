d190af703e88fb09f0dadc2ec3380d1fe3c781c5
Ivana Djordjevic <127266659+runningaroundintheabyss@users.noreply.github.com>
2023-12-06 21:46:33 -0800

    database still not functional, one less error compared to previously

--- db/schema.sql
+++ db/schema.sql
@@ -12,8 +12,9 @@ CREATE TABLE role (
     id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id: INT NOT NULL,
-    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL 
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES department(id) 
+    -- ON DELETE SET NULL gives an error here
 );
 
 CREATE TABLE employee (
