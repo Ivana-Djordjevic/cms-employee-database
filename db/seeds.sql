cddefcaf7eb6f5616ccb419f66f3eab261b55157
Ivana Djordjevic <127266659+runningaroundintheabyss@users.noreply.github.com>
2023-12-06 23:47:13 -0800

    database is functional, added sql queries (they are indepentent and not connected to anything as of now)

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,44 +1,44 @@
 USE work_db;
 
 INSERT INTO department (name) VALUES 
-                        ('finance'), --1 
-                        ('legal'), --2 
-                        ('human resources'), --3
-                        ('engineering'), --4
-                        ('project management'), --5
-                        ('quality assurance'); --6
+                        ('finance'), 
+                        ('legal'), 
+                        ('human resources'),
+                        ('engineering'),
+                        ('project management'),
+                        ('quality assurance');
 
 INSERT INTO role (title, salary, department_id) VALUES
-                ('financial analyst', 70000.00, 1), --1
-                ('accountant', 70000.00, 1), --2
-                ('legal scretary', 40000.00, 2), --3
-                ('attorney', 80000.00, 2), --4
-                ('recruiter', 30000.00, 3), --5
-                ('HR coordinator', 40000.00, 3), --6
-                ('HR manager', 60000.00, 3), --7
-                ('computer engineer', 80000.00, 4), --8
-                ('mechanical engineer', 72000.00, 4), --9
-                ('chemical engineer', 60000.00, 4), --10
-                ('director', 100000.00, 4), --11
-                ('director', 100000.00, 5), --12
-                ('project manager', 66000.00, 5), --13
-                ('quality director', 120000.00, 6), --14
-                ('quality analysit', 40000.00, 6); --15
+                ('financial analyst', 70000.00, 1),
+                ('accountant', 70000.00, 1),
+                ('legal scretary', 40000.00, 2),
+                ('attorney', 80000.00, 2),
+                ('recruiter', 30000.00, 3),
+                ('HR coordinator', 40000.00, 3),
+                ('HR manager', 60000.00, 3),
+                ('computer engineer', 80000.00, 4),
+                ('mechanical engineer', 72000.00, 4),
+                ('chemical engineer', 60000.00, 4), 
+                ('director', 100000.00, 4), 
+                ('director', 100000.00, 5), 
+                ('project manager', 66000.00, 5),
+                ('quality director', 120000.00, 6), 
+                ('quality analysit', 40000.00, 6); 
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
-                        ('Stella', 'Bella', 1, 101), --1
-                        ('Emma', 'Mella', 2, 101), --2
-                        ('Donna', 'Tella', 3, 101), --3
-                        ('Maya', 'Zara', 4, 101), --4
-                        ('Clara', 'Luna', 5, 101), --5
-                        ('Ava', 'Gemma', 6, 101), --6
-                        ('Layla', 'Nova', 7, null), --7
-                        ('Mira', 'Tala', 8, 201), --8
-                        ('Anya', 'Lara', 9, 201), --9
-                        ('Neva', 'Gina', 10, 201), --10
-                        ('Flora', 'Myra', 11, null), --11
-                        ('Sasha', 'Yara', 12, null), --12
-                        ('Nalla', 'Lyra', 13, 301), --13
-                        ('Ella', 'Rhea', 14, 301), --14
-                        ('Kara', 'Zora', 15, 301); --15
+                        ('Stella', 'Bella', 1, 101),
+                        ('Emma', 'Mella', 2, 101),
+                        ('Donna', 'Tella', 3, 101),
+                        ('Maya', 'Zara', 4, 101),
+                        ('Clara', 'Luna', 5, 101),
+                        ('Ava', 'Gemma', 6, 101),
+                        ('Layla', 'Nova', 7, null),
+                        ('Mira', 'Tala', 8, 201),
+                        ('Anya', 'Lara', 9, 201),
+                        ('Neva', 'Gina', 10, 201), 
+                        ('Flora', 'Myra', 11, null), 
+                        ('Sasha', 'Yara', 12, null), 
+                        ('Nalla', 'Lyra', 13, 301), 
+                        ('Ella', 'Rhea', 14, 301), 
+                        ('Kara', 'Zora', 15, 301); 
 
--- index.js
+++ index.js
@@ -3,6 +3,72 @@ const mysql = require('mysql2');
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '',
-    database: '',
+    password: 'random9!',
+    database: 'work_db',
 })
+
+db.query('SELECT * FROM department;', (err, result) => {
+    err ? console.log(err) : console.log(result)}
+);
+
+db.query('SELECT * FROM role;', (err, result) => {
+    err ? console.log(err) : console.log(result)}
+);
+
+db.query(`SELECT employee.id, employee.first_name, employee.last_name, role.title AS job_title, role.salary, department.name AS department, employee.manager_id
+            FROM employee 
+            JOIN role 
+            ON employee.role_id = role.id 
+            JOIN department 
+            ON role.department_id = department.id;`, (err, result) => {
+    err ? console.log(err) : console.log(result)}
+);
+
+db.query('INSERT INTO department (name) VALUES (?);', 'placeholder' ,(err, result) => {
+    err ? console.log(err) : console.log(result)}
+);
+
+db.query('INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?);', ['placeholder','10', '10' ] ,(err, result) => {
+    err ? console.log(err) : console.log(result)}
+);
+
+db.query('INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?);', ['placeholder','placeholder', '10', '10' ] ,(err, result) => {
+    err ? console.log(err) : console.log(result)}
+);
+
+// combines both tables togethether 
+
+// SELECT * FROM employee 
+// JOIN role 
+// ON employee.role_id = role.id;
+
+// combines wanted columns from both tables
+
+// SELECT employee.id, employee.first_name, employee.last_name, employee.manager_id, role.title, role.salary 
+// FROM employee 
+// JOIN role  
+// ON employee.role_id = role.id;
+
+//selects what we wanna see for the employee
+/* 
+
+SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, department.name, employee.manager_id
+FROM employee 
+JOIN role 
+ON employee.role_id = role.id 
+JOIN department 
+ON role.department_id = department.id;
+
+
+//selects what we wanna see for the employee with proper column names
+/* 
+
+SELECT employee.id, employee.first_name, employee.last_name, role.title AS job_title, role.salary, department.name AS department, employee.manager_id
+FROM employee 
+JOIN role 
+ON employee.role_id = role.id 
+JOIN department 
+ON role.department_id = department.id;
+
+*/
+
