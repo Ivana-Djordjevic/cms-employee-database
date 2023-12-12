e9939e7011024cc430950646029faa39d2bed832
Ivana Djordjevic <127266659+runningaroundintheabyss@users.noreply.github.com>
2023-12-11 21:34:38 -0800

    changed my sql query to sql .promise(), no more callback hell

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const inquirer = require('inquirer');
 
 const { questions, departmentInfo, roleInfo, employeeInfo, employeeUpdate, employeeManagerUpdate, employeeDeletion, roleDeletion, deparmentDeletion } = require('./lib/inquire');
-const { renderDepartmentData, renderRoleData, renderEmployeeData, addAdepartment, addArole, AddEmployee, determineRoleId, updateEmployee, determineEmployeedID, updateEmployeesManager, renderEmployeesByManagers, renderEmployeesByDepartment, renderBudgetOfEachDepartment, deleteEmployee, deleteRoleAndItsEmployees, determineDepartmentId, deleteEntireDepartment } = require('./lib/render')
+const { renderDepartmentData, renderRoleData, renderEmployeeData, addAdepartment, addArole, addEmployee, determineRoleId, updateEmployee, determineEmployeedID, updateEmployeesManager, renderEmployeesByManagers, renderEmployeesByDepartment, renderBudgetOfEachDepartment, deleteEmployee, deleteRoleAndItsEmployees, determineDepartmentId, deleteEntireDepartment } = require('./lib/render')
 
 //returns user's inquiries
 async function init() {
@@ -11,17 +11,21 @@ async function init() {
         const answers = await inquirer.prompt(questions);
 
         if (answers.start === 'view all departments') {
-            renderDepartmentData(init);
+            await renderDepartmentData();
+            init();
         } else if (answers.start === 'view all roles') {
-            renderRoleData(init);
+            await renderRoleData();
+            init();
         } else if (answers.start === 'view all employees') {
-            renderEmployeeData(init);
+            await renderEmployeeData();
+            init();
         } else if (answers.start === 'add a department') {
             const addDepartment = await inquirer.prompt(departmentInfo);
 
             const department = addDepartment.department;
 
-            addAdepartment(department, init);
+            await addAdepartment(department);
+            init();
         } else if (answers.start === 'add a role') {
             const role = await inquirer.prompt(roleInfo);
 
@@ -29,7 +33,8 @@ async function init() {
             const salary = parseFloat(role.salary);
             const departmentName = role.department;
 
-            addArole(title, salary, departmentName, init);
+            await addArole(title, salary, departmentName);
+            init();
         } else if (answers.start === 'add an employee') {
             const employee = await inquirer.prompt(employeeInfo);
 
@@ -37,56 +42,59 @@ async function init() {
             const lastName = employee.lastName;
             const role = employee.role;
             const manager = employee.manager;
-            determineRoleId(role, (roleId) => {
-                AddEmployee(firstName, lastName, roleId, manager, init);
-            });
 
+            const roleId = await determineRoleId(role);
+            await addEmployee(firstName, lastName, roleId, manager);
+            init();
         } else if (answers.start === 'update an employee role') {
             const name = await inquirer.prompt(employeeUpdate);
 
             const employeeName = name.employee;
             const role = name.role;
-            determineEmployeedID(employeeName, (employeeId) => {
-                determineRoleId(role, (roleId) => {
-                    updateEmployee(roleId, employeeId, init);
-                });
-            });
+
+            const employeeId = await determineEmployeedID(employeeName);
+            const roleId = await determineRoleId(role);
+            await updateEmployee(roleId, employeeId);
+            init();
         } else if (answers.start === 'update an employee\'s manager') {
             const managerUpdate = await inquirer.prompt(employeeManagerUpdate);
 
             const employeeName = managerUpdate.employee; 
             const newManager = managerUpdate.manager;  
-            determineEmployeedID(employeeName, (employeeId) => {
-               updateEmployeesManager(newManager, employeeId, init);     
-           });
-
+            const employeeId = await determineEmployeedID(employeeName);
+            await updateEmployeesManager(newManager, employeeId);     
+            init();
         } else if (answers.start === 'view employees by managers') {
-            renderEmployeesByManagers(init);
+            await renderEmployeesByManagers();
+            init();
         } else if (answers.start === 'view employees by department') {
-            renderEmployeesByDepartment(init);
+            await renderEmployeesByDepartment();
+            init();
         } else if (answers.start === 'view total budget of each department') {
-            renderBudgetOfEachDepartment(init);
+            await renderBudgetOfEachDepartment();
+            init();
         } else if (answers.start === 'delete an employee') {
             const employeeToDelete = await inquirer.prompt(employeeDeletion);
 
             const employeeName = employeeToDelete.employee;
-            determineEmployeedID(employeeName, (employeeId) => {
-                deleteEmployee(employeeId, init);
-            });
+
+            const employeeId = await determineEmployeedID(employeeName);
+            await deleteEmployee(employeeId);
+            init();
         } else if (answers.start === 'delete a role and its employee(s)') {
             const roleToDelete = await inquirer.prompt(roleDeletion);
 
             const role = roleToDelete.role;
-            determineRoleId(role, (roleId) => {
-                deleteRoleAndItsEmployees(roleId, init);
-            });
+            const roleId = await determineRoleId(role);
+            await deleteRoleAndItsEmployees(roleId);
+            init();
         } else if (answers.start === 'delete an entire department') {
             const departmentToDelete = await inquirer.prompt(deparmentDeletion);
             
             const department = departmentToDelete.department;
-            determineDepartmentId(department, (departmentId) => {
-                deleteEntireDepartment(departmentId, init)
-            });
+            const departmentId = await determineDepartmentId(department);
+            await deleteEntireDepartment(departmentId)
+            init();
         } else if (answers.start === 'quit') {
             process.exit(0);
         }
